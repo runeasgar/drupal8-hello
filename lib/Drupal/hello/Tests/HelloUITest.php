9b88e2aac8e7f8a0cd2e6fdb0ccd653caf075135
Joshua Taylor <joshua.taylor@acquia.com>
2013-10-27 20:43:32 -0400

    all simpletests working

--- lib/Drupal/hello/Tests/HelloUITest.php
+++ lib/Drupal/hello/Tests/HelloUITest.php
@@ -30,46 +30,41 @@ class HelloUITest extends WebTestBase {
   function setUp() {
     parent::setUp();
     $this->web_user = $this->drupalCreateUser(array('be greeted'));
-    $this->admin_user = $this->drupalCreateUser(array('administer blocks', 'administer hello'));
+    $this->admin_user = $this->drupalCreateUser(array('be greeted', 'administer blocks', 'administer hello'));
   }
 
   function testHelloHome() {
     $this->drupalLogin($this->web_user);
-    $this->drupalGet("hello");
-    $this->assertText('Hello, ' $this->web_user->getUsername());
+    $this->drupalGet('hello');
+    $this->assertText('Hello, ' . $this->web_user->getUsername());
   }
 
   function testHelloBlock() {
     $this->drupalLogin($this->admin_user);
     $this->drupalPlaceBlock('hello_block');
-    $this->drupalGet("/");
+    $this->drupalGet('');
     $this->assertText('Hello, ' . $this->admin_user->getUsername());
   }
 
   function testHelloAdmin() {
     $this->drupalLogin($this->admin_user);
+
     $this->drupalGet('admin/config/hello/settings');
-    $this->assertFieldByName('show_username', '0');
-    $this->drupalGet("hello");
-    $this->assertText('Hello, world!'); // STOPPED HERE
-    $this->drupalGet('admin/config/hello/settings');
-    $this->drupalPostForm(NULL, array('show_username' => '1'), t('Save configuration'));
-    $this->assertFieldByName('show_username', '1');
-    $this->drupalGet("hello");
+    //$this->assertFieldByName('show_username', '1');
+    $this->assertFieldChecked('edit-show-username');
+    $this->drupalGet('hello');
     $this->assertText('Hello, ' . $this->admin_user->getUsername());
+
     $this->drupalPlaceBlock('hello_block');
-    $this->drupalGet("/");
-    $this->assertText('Hello, ' .  . $this->admin_user->getUsername());
-  }
+    $this->drupalGet("");
+    $this->assertText('Hello, ' . $this->admin_user->getUsername());
 
-  function testPantsConfiguration() {
-    $this->drupalLogin($this->admin_user);
+    $this->drupalGet('admin/config/hello/settings');
+    $this->drupalPostForm(NULL, array('show_username' => false), t('Save configuration'));
+    $this->assertNoFieldChecked('edit-show-username');
+    $this->drupalGet("hello");
+    $this->assertText('Hello, world!');
 
-    $this->drupalGet('admin/config/people/pants');
-    $this->assertFieldByName('pants_type', '');
-
-    $this->drupalPostForm(NULL, array('pants_type' => 'bellbottoms'), t('Save configuration'));
-    $this->assertFieldByName('pants_type', 'bellbottoms');
   }
 
 }
